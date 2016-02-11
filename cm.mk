ce8d274170444cc4b8ceb43ccf3d3909c57e9ea5
Nikita <nikich340@gmail.com>
2016-02-12 08:15:33 +1000

    Drop full_<device>.mk
    Change-Id: I90fe47304d52158b104e6b1101daf3a97cee9daf

--- cm.mk
+++ cm.mk
@@ -1,5 +1,7 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/lge/jagnm/device_jagnm.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -8,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 1280
 # Release name
 PRODUCT_RELEASE_NAME := LG G3s
 PRODUCT_NAME := cm_jagnm
-PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := LG-D722
-
-$(call inherit-product, device/lge/jagnm/full_jagnm.mk)
+PRODUCT_DEVICE := jagnm
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
