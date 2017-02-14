c1494663ac9238d745f53de4b51a14737402359b
vm03 <vasy@vasy.ru>
2017-02-14 19:03:16 +0300

    cmactions: build it
    Change-Id: I569b4702feb4474b35184873dd99af2987c1b56b

--- device_jagnm.mk
+++ device_jagnm.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
     brcm-uim-sysfs \
     libfmjni
 
+# CMActions
+PRODUCT_PACKAGES += \
+    CMActions
+
 # Inherit from msm8226-common
 $(call inherit-product, device/lge/msm8226-common/msm8226.mk)
 $(call inherit-product, vendor/lge/jagnm/jagnm-vendor.mk)
